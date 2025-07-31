fdb5e11b4727472f9f58cf66328e97310eea3b0c
Ramone Wynter <hi@arrdublu.us>
2025-07-31 18:08:05 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore, type Firestore } from 'firebase/firestore';
 import { getAuth, type Auth } from 'firebase/auth';
 
 const firebaseConfig = {
-  projectId: "arrdublu-yur7j",
-  appId: "1:661883279745:web:0740f986e7070b85f6f080",
-  storageBucket: "arrdublu-yur7j.firebasestorage.app",
-  apiKey: "AIzaSyAsMyiK2r5v_iAJaof_9AaDsdBkr9tZkDA",
-  authDomain: "arrdublu-yur7j.firebaseapp.com",
-  messagingSenderId: "661883279745"
+  "projectId": "arrdublu-yur7j",
+  "appId": "1:661883279745:web:0740f986e7070b85f6f080",
+  "storageBucket": "arrdublu-yur7j.firebasestorage.app",
+  "apiKey": "AIzaSyAsMyiK2r5v_iAJaof_9AaDsdBkr9tZkDA",
+  "authDomain": "arrdublu-yur7j.firebaseapp.com",
+  "messagingSenderId": "661883279745"
 };
 
 const app: FirebaseApp = getApps().length ? getApp() : initializeApp(firebaseConfig);
