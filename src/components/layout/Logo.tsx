704814f93c9aae2bc46eb5b42f717f54f5a023d5
Ramone Wynter <hi@arrdublu.us>
2025-08-05 15:14:20 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/components/layout/Logo.tsx
+++ src/components/layout/Logo.tsx
@@ -4,11 +4,12 @@ import Image from 'next/image';
 export function Logo() {
   return (
     <Image
-      src="https://firebasestorage.googleapis.com/v0/b/arrdublu-3.firebasestorage.app/o/arrdublu_logo_blackArtboard-1.png?alt=media&token=0c573884-0b4b-405c-ba36-5477f43a1699"
-      width={150}
-      height={40}
+      src="https://firebasestorage.googleapis.com/v0/b/arrdublu-3.firebasestorage.app/o/Header_Starting_Mobile_Logo.png?alt=media&token=36c8d541-3b34-4458-8dc5-1ac793933e5b"
+      width={135}
+      height={36}
       alt="Arrdublu Logo"
       priority
+      className="object-contain"
     />
   );
 }
