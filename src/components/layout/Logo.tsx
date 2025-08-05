fddafd1e45f5ee3d340604cb1ff96cf2d5b8a83a
Ramone Wynter <hi@arrdublu.us>
2025-08-05 19:10:26 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/components/layout/Logo.tsx
+++ src/components/layout/Logo.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 export function Logo() {
   const lightLogo = "https://firebasestorage.googleapis.com/v0/b/arrdublu-3.firebasestorage.app/o/arrdublu_logo_blackArtboard-1.png?alt=media&token=0c573884-0b4b-405c-ba36-5477f43a1699";
-  const darkLogo = "https://firebasestorage.googleapis.com/v0/b/arrdublu-yur7j.appspot.com/o/arrdublu_logo_whiteArtboard-1.png?alt=media&token=08940c38-8959-450f-8b93-9028aed51152";
+  const darkLogo = "https://firebasestorage.googleapis.com/v0/b/arrdublu-3.firebasestorage.app/o/Header_Starting_Mobile_Logo.png?alt=media&token=36c8d541-3b34-4458-8dc5-1ac793933e5b";
 
   return (
     <div className="relative h-9 w-36">
