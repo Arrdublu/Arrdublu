ac7838a767c4df12542e521fe5590c7c13af34b5
Ramone Wynter <hi@arrdublu.us>
2025-08-05 13:51:13 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/components/layout/Cart.tsx
+++ src/components/layout/Cart.tsx
@@ -11,18 +11,18 @@ export function Cart() {
   const { cartCount } = useCart();
 
   return (
-    <Button variant="ghost" size="icon" asChild>
-      <Link href="/cart">
-        <div className="relative">
-          <ShoppingBag />
-          {cartCount > 0 && (
-            <span className="absolute -top-2 -right-2 bg-primary text-primary-foreground text-xs rounded-full h-5 w-5 flex items-center justify-center">
-              {cartCount}
-            </span>
-          )}
-        </div>
-        <span className="sr-only">Shopping Bag</span>
+      <Link href="/cart" passHref>
+        <Button variant="ghost" size="icon" asChild>
+          <div className="relative">
+            <ShoppingBag />
+            {cartCount > 0 && (
+              <span className="absolute -top-2 -right-2 bg-primary text-primary-foreground text-xs rounded-full h-5 w-5 flex items-center justify-center">
+                {cartCount}
+              </span>
+            )}
+             <span className="sr-only">Shopping Bag</span>
+          </div>
+        </Button>
       </Link>
-    </Button>
   );
 }
